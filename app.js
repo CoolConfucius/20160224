7369a8e6700a28b9d26f0d3ca45e5f1dbb28164c
Benjamin Huang <coolconfucius@yahoo.com>
2016-02-24 12:07:52 -0800

    input working

--- app.js
+++ app.js
@@ -3,11 +3,32 @@ import { render } from 'react-dom';
 import MyInput from 'components/input'; 
 
 class Greeting extends Component {
+  constructor(props) {
+    super(props); 
+
+    // this.state = { greeting: 'Hello World' };
+    this.state = { 
+      greetingOne: 'Hello World', 
+      greetingTwo: 'Not going to change'
+    };
+  }
+
+  changeGreeting(value) {
+    // this.setState({ greeting: value }); 
+    this.setState({ greetingOne: value }); 
+    console.log(this.state);
+  }
+
+  revertGreeting() {
+    this.setState({ greetingOne: 'Hello World' }); 
+  }
+
   render(){
     return (
       <div>
-        Hello World!!!!
-        <MyInput />
+        { this.state.greetingOne }
+        <MyInput  revertGreeting={this.revertGreeting.bind(this)} 
+                  changeGreeting={this.changeGreeting.bind(this)}/>
       </div>
     );
   }
--- components/input.js
+++ components/input.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'; 
 
 class MyInput extends Component {
+  constructor(props) {
+    super(props); 
+
+    this.state = { text: '' };
+  }
+
+  changeText(e) {
+    // console.log('event:', e);
+    // console.log('event:', e.target.value);
+    this.setState({ text: e.target.value }); 
+  }
+
+  click() {
+    console.log('Hello There');
+    console.log('State: ', this.state);
+    this.props.changeGreeting(this.state.text)
+  }
+
   render() {
     return (
       <div>
-        <input type="text"/>
-        <button>Click Me!</button>
+        <input type="text" onChange={this.changeText.bind(this)} />
+        <button onClick={this.props.changeGreeting.bind(this, this.state.text)}>Click Me!</button>
+        <button onClick={this.props.revertGreeting} >Revert</button>
       </div>
     );
   }
